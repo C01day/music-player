78a9fe6ef8d7b696318f3965b2c53862b3ae0ca3
C01day <ddaweizhou@gmail.com>
2022-05-30 23:38:59 +0800

    解决静态资源不显示问题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: "./",
+  //  构建时的输出目录
+  outputDir: "dist",
+  //  放置静态资源的目录
+  assetsDir: "static",
+  //  html 的输出路径
+  indexPath: "index.html",
+  //文件名哈希
+  filenameHashing: true,
+  lintOnSave: false,
+  //  是否使用带有浏览器内编译器的完整构建版本
+  runtimeCompiler: false,
+
 })
