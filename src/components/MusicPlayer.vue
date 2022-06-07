9e300190843b709e7e871c79bfd846d22f0548e4
C01day <ddaweizhou@gmail.com>
2022-06-07 16:25:51 +0800

    update regular

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -1,13 +1,8 @@
 <template>
 <div id="box">
-	<a href="https://github.com/C01day/music-player" class="github-corner" aria-label="View source on GitHub" target="_blank">
-		<svg width="80" height="80" viewBox="0 0 250 250" style="fill:#FF0000; color:#FFF; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true">
-			<path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
-			<path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>
-			<path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
-		</svg>
-	</a>
+	<GithubCorner class="github-corner-out"></GithubCorner>
 	<div class="audioPlayer">
+		<GithubCorner class="github-corner-in"></GithubCorner>
 		<a class="nav-icon" @click="isPlaylistActive=!isPlaylistActive" :class="{'isActive': isPlaylistActive}" title="Music List">
 				<span></span>
 				<span></span>
@@ -79,8 +74,12 @@
 </template>
 
 <script>
+import GithubCorner from './GithubCorner.vue'
 export default {
 	name: "MusicPlayer",
+	components: {
+		GithubCorner,
+	},
 	data() {
 		return {
 				audio: "",
@@ -488,6 +487,10 @@ export default {
 	box-sizing border-box
 .animated 
 	animation-duration 0.5s
+.github-corner-out
+	display none
+.github-corner-in
+	display inline-block
 #box
 	margin 0 auto
 	width 100%
@@ -608,7 +611,8 @@ export default {
 						color rgba(0, 0, 0, 0.75)
 						transition-delay 100ms
 				.page-container
-					display none
+					// display none
+					display inline-block
 					margin 0.2rem 0
 					position relative
 					background-color #FFF
@@ -795,22 +799,11 @@ export default {
 	100% 
 		transform scaleY(0)
 
-.github-corner:hover .octo-arm
-	animation octocat-wave 560ms ease-in-out
-@keyframes octocat-wave
-	0%,100%
-		transform:rotate(0)
-	20%,60%
-		transform:rotate(-25deg)
-	40%,80%
-		transform:rotate(10deg)
-@media (max-width:500px)
-	.github-corner:hover .octo-arm
-		animation none
-	.github-corner .octo-arm
-		animation:octocat-wave 560ms ease-in-out
-
 @media (min-width: 719px)
+	.github-corner-out
+		display inline-block
+	.github-corner-in
+		display none
 	.audioPlayerUI
 		display grid
 		grid-template-columns 1fr 1fr
